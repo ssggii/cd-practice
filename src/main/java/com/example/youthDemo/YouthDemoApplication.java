a614d0de720fb897919abd4205551d6b33e82e69
ssggii <haan7525@gmail.com>
2024-05-19 18:18:30 +0900

    Initial commit

---
+++ src/main/java/com/example/youthDemo/YouthDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.youthDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouthDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouthDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/youthDemo/YouthDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.youthDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouthDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
